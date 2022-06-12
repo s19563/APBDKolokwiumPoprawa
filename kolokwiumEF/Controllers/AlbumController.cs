0bc43d75b6354a489353063169933a0f60739772
s19563 <l0372@pjwstk.edu.pl>
2022-06-12 11:09:56 +0200

    commit

---
+++ kolokwiumEF/Controllers/AlbumController.cs
@@ -0,0 +1,38 @@
+﻿using kolokwiumEF.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolokwiumEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        private readonly IAlbumsDBService _albumsDBService;
+        public AlbumController(IAlbumsDBService albumsDBService)
+        {
+            _albumsDBService = albumsDBService;
+        }
+        /*
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetAlbum(int idAlbum)
+        {
+            try
+            {
+                if(idAlbum > 0)
+                {
+                    return Ok(await _albumsDBService.GetAlbum(idAlbum));
+                }
+            }
+            catch (System.Exception)
+            {
+                return Conflict();
+            }
+
+        }
+        */
+    }
+}
